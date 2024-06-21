ddd4509b1ddf831e193663ea1bba475589ae73fe
Snithesh <snitheshg30@gmail.com>
2024-06-21 12:19:45 +0530

    first changes

--- Snithesh_Portfolio_main/src/components/journey/Journey.jsx
+++ Snithesh_Portfolio_main/src/components/journey/Journey.jsx
@@ -9,7 +9,7 @@ const Journey = () => {
       className="w-full py-20 border-b-[1px] border-b-black"
     >
       <div className="flex justify-center items-center text-center">
-        <Title title="My Learning Journey" des="My Journey" />
+        <Title des="My Journey" />
       </div>
 
       <Education />
