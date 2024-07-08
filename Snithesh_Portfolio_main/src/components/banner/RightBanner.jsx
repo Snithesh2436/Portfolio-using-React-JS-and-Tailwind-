e221c0cfa4b39c4cf662944c99526b4318857952
Snithesh <snitheshg30@gmail.com>
2024-07-08 22:35:49 +0530

    first changes

--- Snithesh_Portfolio_main/src/components/banner/RightBanner.jsx
+++ Snithesh_Portfolio_main/src/components/banner/RightBanner.jsx
@@ -5,7 +5,7 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[300px] h-[300px] lgl:w-[500px] lgl:h-[530px] z-10"
+        className="w-[300px] h-[300px] lgl:w-[500px] lgl:h-[530px] z-10 rounded-full"
         src={bannerImg}
         alt="bannerImg"
       />
